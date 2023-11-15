9a829361d21040ae56481ec94685dc37a22d78e5
Valen-Qiu <jiayunqiu@ocadu.ca>
2023-11-14 21:44:43 -0500

    p3

--- projects/interaction/script.js
+++ projects/interaction/script.js
@@ -21,3 +21,28 @@ $(document).ready(function () {
     });
 });
 
+//interactive background
+document.addEventListener('DOMContentLoaded', () => {
+    const interBubble = document.querySelector('.interactive');
+    let curX = 0;
+    let curY = 0;
+    let tgX = 0;
+    let tgY = 0;
+
+    function move() {
+        curX += (tgX - curX) / 20;
+        curY += (tgY - curY) / 20;
+        interBubble.style.transform = `translate(${Math.round(curX)}px, ${Math.round(curY)}px)`;
+        requestAnimationFrame(() => {
+            move();
+        });
+    }
+
+    window.addEventListener('mousemove', (event) => {
+        tgX = event.clientX;
+        tgY = event.clientY;
+    });
+
+    move();
+});
+
